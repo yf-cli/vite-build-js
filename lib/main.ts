c9cab62e87a2c96c6d3cf09ef5d81f2590ec3146
Tmier <itmier@126.com>
2023-02-15 10:43:41 +0800

    feat: 项目初始化

---
+++ lib/main.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: 王云飞
+ * @Date: 2023-02-13 14:50:43
+ * @LastEditTime: 2023-02-15 10:02:18
+ * @LastEditors: 王云飞
+ * @Description: 
+ * 
+ */
+import './aaa.scss'
+export const a = 2

36a7f919b707413b1c45105ad2d0f7d769e60ceb
Tmier <itmier@126.com>
2023-02-24 09:50:41 +0800

    feat: 更新模板

--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: 王云飞
  * @Date: 2023-02-13 14:50:43
- * @LastEditTime: 2023-02-15 10:02:18
+ * @LastEditTime: 2023-02-23 16:44:40
  * @LastEditors: 王云飞
- * @Description: 
- * 
+ * @Description:
+ *
  */
 import './aaa.scss'
 export const a = 2
